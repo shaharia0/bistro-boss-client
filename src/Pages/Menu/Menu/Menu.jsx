4e35ef90a0943ce1ab8b2803fb5b004d1b28aabd
shaharia0 <shaharia35-1964@diu.edu.bd>
2023-06-06 01:58:02 +0600

    added our menu page sections

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -1,16 +1,40 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Shared/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg'
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg'
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg'
+import saladImg from '../../../assets/menu/salad-bg.jpg'
+import soupImg from '../../../assets/menu/soup-bg.jpg'
+import useMenu from '../../../Hooks/useMenu';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
 
 const Menu = () => {
+  const [menu]= useMenu();
+  const desserts = menu.filter(item => item.category === 'dessert');
+  const soups = menu.filter(item => item.category === 'soup');
+  const pizzas = menu.filter(item => item.category === 'pizza');
+  const salads = menu.filter(item => item.category === 'salad');
+  const offered = menu.filter(item => item.category === 'offered');
+  
   return (
     <div>
       <Helmet>
         <title>Bistro | Menu</title>
       </Helmet>
+      {/* main cover */}
       <Cover img={menuImg} title="Our Menu"></Cover>
-      <PopularMenu></PopularMenu>
+      <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+      {/* offered menu items */}
+       <MenuCategory items={offered}></MenuCategory>
+       {/* desert menu items  */}
+       <MenuCategory items={desserts} title="Dessert" coverImg={dessertImg}></MenuCategory>
+       {/* pizza menu items */}
+       <MenuCategory items={pizzas} title="Pizza" coverImg={pizzaImg}></MenuCategory>
+       {/* salad menu items */}
+       <MenuCategory items={salads} title="Salad" coverImg={saladImg}></MenuCategory>
+       {/* soup menu items */}
+       <MenuCategory items={soups} title="Soup" coverImg={soupImg}></MenuCategory>
       <h1>menu will be here</h1>
     </div>
   );
